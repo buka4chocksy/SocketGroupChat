027af07dc8763007a67fc053c94039723bfb2a3b
buka4chocksy <buka4chocksy@gmail.com>
2020-03-02 16:41:47 +0100

    modified file name

--- bin/www.js
+++ bin/www.js
@@ -10,7 +10,7 @@ var path = require('path');
 var fs = require('fs');
 var http = require('http');
 // var normalizePort = require('normalize-port');
-const initSocket = require("../app/Socket").indexSocket;
+const initSocket = require("../app/socket").indexSocket;
 
 global.activeSocketUsers = []
 /**
